989ad0f365b683e6162ed390a22adfb4286ae59b
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-11-18 11:23:12 +0400

    new specs

---
+++ generation_spec/cda_gen_spec.rb
@@ -0,0 +1,44 @@
+require_relative 'gen_spec_helper'
+
+describe Gen do
+  include described_class
+
+  before(:all) do
+    require 'virtus'
+    generate
+    $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+    require 'cda'
+  end
+
+  it "should not generate cda classes for mapped types" do
+    File.should_not exist("#{output_path}/date.rb")
+    File.should_not exist("#{output_path}/integer.rb")
+  end
+
+  it 'should generat at least Act and Section' do
+    expect do
+      Dir["#{output_path}/**/*rb"].each do |file|
+        next if file =~ /autoloads/
+        require file
+      end
+
+      Cda::Act
+      Cda::Section
+    end.not_to raise_error
+  end
+
+  it 'should generate hierarchy of classes' do
+    Cda::PIVL_TS.should < Cda::SXCM_TS
+    Cda::ST.should < Cda::ED
+  end
+
+  context 'Cda::Observation' do
+    it 'should be generated' do
+      expect { Cda::Observation }.not_to raise_error
+    end
+
+    it 'should generate at least value attributes for observation' do
+      Cda::Observation.new.should respond_to(:value)
+    end
+  end
+end
---
+++ spec/cda/base_types_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "CDA Base types" do
+  it "should map CDA types to basic ruby types" do
+    Cda::TS.attribute_set[:value].type.primitive.should == Date
+    Cda::REAL.attribute_set[:value].type.primitive.should == Float
+    Cda::INT.attribute_set[:value].type.primitive.should == Integer
+  end
+end
